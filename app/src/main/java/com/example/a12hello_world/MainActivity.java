2bb3660252138c79d936299c31f00eaf92b3f559
Alexander Shapovalov <avshapovalov90@mail.ru>
2019-03-09 12:57:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a12hello_world/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a12hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+        final EditText editName = findViewById(R.id.editTextName);
+        final EditText editEmail= findViewById(R.id.editTextEmail);
+        Button ok = findViewById(R.id.ok);
+        Button clear = findViewById(R.id.clear);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Подписка на рассылку успешно оформлена для пользователя: " + editName.getText()
+                        + "на электронный адрес: " + editEmail.getText());
+
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editEmail.setText("");
+                editName.setText("");
+                textView.setText("");
+            }
+        });
+
+    }
+}
